6645c3525e2cbeaff523fdbc74d639298138df28
ouckah <aidan.ouckama@gmail.com>
2025-02-19 10:38:19 -0500

    Initial Commit

---
+++ Pluck/PluckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluckApp.swift
+//  Pluck
+//
+//  Created by Aidan Ouckama on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PluckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PluckTests/PluckTests.swift
@@ -0,0 +1,17 @@
+//
+//  PluckTests.swift
+//  PluckTests
+//
+//  Created by Aidan Ouckama on 2/19/25.
+//
+
+import Testing
+@testable import Pluck
+
+struct PluckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
